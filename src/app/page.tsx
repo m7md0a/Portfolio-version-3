524b60fa4483c480446394b1781c95472236dc9d
m7md0a <m7md0a.work@gmail.com>
2024-02-28 22:35:34 +0200

    fix build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import About from "@/components/Home/About";
 import Experience from "@/components/Home/Experience";
 import Hero from "@/components/Home/Hero";
 import LatestRepos from "@/components/Home/LatestRepos";
-import MyCom from "@/components/Home/MyCom";
+// import MyCom from "@/components/Home/MyCom";
 import Skills from "@/components/Home/Skills";
 import Thanks from "@/components/Home/Thanks";
 import React from "react";
